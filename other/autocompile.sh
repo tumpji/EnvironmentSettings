0c4eec5607dba0359eaacda16d0d855fa691f98b
Jiří Tumpach <tumpji@gmail.com>
2018-07-05 00:47:06 +0200

    added nonstandard files

---
+++ other/autocompile.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# UTF-8
+# =============================================================================
+#         FILE: autocompile.sh
+#  DESCRIPTION:
+#        USAGE:
+#      OPTIONS:
+# REQUIREMENTS:
+#
+#      LICENCE:
+#
+#         BUGS:
+#        NOTES:
+#       AUTHOR: Jiří Tumpach (tumpji),
+# ORGANIZATION:
+#      VERSION: 1.0
+#      CREATED: 2018 07.03.
+# =============================================================================
+
+
+tested_files=('./chap01.tex' './thesis.tex' './bibliography.bib' './bibliography.tex')
+
+# time delay every check (array in case of not acting for $wait_minutes_change) it wil 
+# inc wait_index to maximum of #wait_option-1
+wait_option=(1 2 3 3 3 3 4 5 6 7 8 9 10)
+wait_index=0
+wait_ticks=0
+wait_minutes_change=1
+
+# last modification time (initial 0)
+last_mod=( $(for i in ${tested_files[*]}; do echo 0; done) )
+# precomputed tick per switch to different controll schedule
+precomputed_wait_time_ticks=( $(for i in ${wait_option[*]}; 
+    do echo $(($wait_minutes_change*60/$i)); done) )
+
+# main loop
+while true; 
+do
+    sleep ${wait_option[$wait_index]} 
+    act_change=no
+
+    # every tested file
+    for ((i=0; $i-${#tested_files[*]}; i+=1))
+    do
+        act_mod=$(stat -c %y ${tested_files[$i]})
+
+        if [ "$act_mod" != "${last_mod[$i]}" ];
+        then
+            echo "$i to $act_mod"
+            last_mod[$i]=$act_mod
+            act_change=yes
+        fi
+    done
+
+    if [ $act_change == no -a $wait_index -ne ${#wait_option[*]} ]
+    then
+        # inc timer
+        wait_ticks=$(($wait_ticks+1))
+        echo "tick"
+
+        # switch schedule
+        if [ $wait_ticks -ge ${precomputed_wait_time_ticks[$wait_index]} ]
+        then
+            wait_index=$((wait_index+1))
+            echo "Changing time schedule to $wait_index]"
+            wait_ticks=0
+        fi
+    elif [ $act_change == yes ]
+    then
+        # reset schedule to most active one
+        wait_index=0
+        wait_ticks=0
+        # recompile
+        make 0<&-
+
+        if [ $? ]
+        then
+            echo "Successfully executed"
+        else
+            echo "Unuccessfully executed"
+        fi
+    fi
+done
