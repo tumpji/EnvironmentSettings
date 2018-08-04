aa6e509237f166eb250a338e5acfa723735d554b
tumpji <tumpji@gmail.com>
2018-08-04 18:03:56 +0200

    Added vundle script

---
+++ other/setUpVimPlugins.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# UTF-8
+# =============================================================================
+#         FILE: setUpVimPlugins.sh
+#  DESCRIPTION:
+#        USAGE:
+#      OPTIONS:
+# REQUIREMENTS:
+#
+#      LICENCE:
+#
+#         BUGS:
+#        NOTES:
+#       AUTHOR: Jiří Tumpach (tumpji),
+# ORGANIZATION:
+#      VERSION: 1.0
+#      CREATED: 2018 08.04.
+# =============================================================================
+
+
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
